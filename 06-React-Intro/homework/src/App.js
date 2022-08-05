29a46b5e3d194dcf53dcb8871118a7291b515452
Agustin Fraile <agustinfraile98@gmail.com>
2022-08-05 13:17:30 -0300

    resolviendo hw 06

--- 06-React-Intro/homework/src/App.js
+++ 06-React-Intro/homework/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
 import SearchBar from './components/SearchBar.jsx';
-import data, { Cairns } from './data.js';
+import data, { Cairns, Londres, Denver } from './data.js';
 
 function App() {
   return (
@@ -15,6 +15,20 @@ function App() {
           name={Cairns.name}
           img={Cairns.weather[0].icon}
           onClose={() => alert(Cairns.name)}
+        />
+        <Card
+          max={Londres.main.temp_max}
+          min={Londres.main.temp_min}
+          name={Londres.name}
+          img={Londres.weather[0].icon}
+          onClose={() => alert(Londres.name)}
+        />
+        <Card
+          max={Denver.main.temp_max}
+          min={Denver.main.temp_min}
+          name={Denver.name}
+          img={Denver.weather[0].icon}
+          onClose={() => alert(Denver.name)}
         />
       </div>
       <hr />
