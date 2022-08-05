65af3f1957d4a65bd82b462aedb2e44d0f4867da
Agustin Fraile <agustinfraile98@gmail.com>
2022-08-05 14:05:17 -0300

    resolviendo hw 07

--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import estilos from '../components/Card.module.css'
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <>
+      <div className = {estilos.contenedor}>
+        <input type="text" placeholder= "city ..." />
+        <button onClick={() => props.onSearch("Buscando...")} className = {estilos.btn}>Search</button>
+      </div>
+    </>
+  )
 };
